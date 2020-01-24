75d9d1d96c20717866ffa93a5fca0870102464ef
burzychs <burzychs@gmail.com>
2020-01-24 10:48:23 +0100

    add review_slider functions && 404 page content

--- workOn_playground/public/scripts/app.js
+++ workOn_playground/public/scripts/app.js
@@ -3,7 +3,8 @@ const App = (function() {
     let self = {};
 
     let state = {
-        openMenu: false
+        openMenu: false,
+        review_slider_status: 0
     }
 
     self.init = function() {
@@ -13,11 +14,40 @@ const App = (function() {
         const hamburger_icon = document.querySelector('#hamburger_icon');
         const close_icon = document.querySelector('#close_icon');
         const arrow_down_icon = document.querySelector('#arrow_down_icon');
+        const review_slider_prev = document.querySelector('.review_slider--prev');
+        const review_slider_next = document.querySelector('.review_slider--next');
+        const list_of_quotes = [...document.querySelectorAll('.home_quote_paragraph')];
 
         // EVENTS
         hamburger_icon.onclick = event => self.handleMenu(event, state);
         close_icon.onclick = event => self.handleMenu(event, state);
         arrow_down_icon.onclick = event => self.scrollToMain(event);
+
+        if (review_slider_prev && review_slider_next) {
+            review_slider_prev.onclick = event => self.handleReviewSliderPrev(event, state, list_of_quotes);
+            review_slider_next.onclick = event => self.handleReviewSliderNext(event, state, list_of_quotes);    
+        }
+    }
+
+    self.handleReviewSliderNext = function(event, state, list) {
+
+        if(state.review_slider_status < list.length - 1) {
+            state.review_slider_status = state.review_slider_status + 1;
+
+            list[state.review_slider_status - 1].style.display = 'none';
+            list[state.review_slider_status].style.display = 'inherit';
+        }
+    }
+
+    self.handleReviewSliderPrev = function(event, state, list) {
+
+        if(state.review_slider_status > 0) {
+            state.review_slider_status = state.review_slider_status - 1;
+
+            list[state.review_slider_status + 1].style.display = 'none';
+            list[state.review_slider_status].style.display = 'inherit';
+        }
+
     }
 
     self.handleMenu = function(event, state) {
